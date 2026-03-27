fe5127ac45cd296988f6b60096aa351d418a0943
TheScubadiver <154598966+TheScubadiver@users.noreply.github.com>
2026-03-27 18:35:11 +0100

    Update sensor.py

--- custom_components/files/sensor.py
+++ custom_components/files/sensor.py
@@ -1,3 +1,5 @@
+"""FileTrack: Custom sensor for Home Assistant, forked for use with Camera Gallery Card."""
+
 import glob
 import logging
 import os
@@ -8,7 +10,7 @@ import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 
-_LOGGER = logging.getLogger(__name__)
+_LOGGER = logging.getLogger(__name__ + ".filetrack")
 
 CONF_FOLDER_PATHS = "folder"
 CONF_FILTER = "filter"
@@ -19,7 +21,7 @@ DEFAULT_FILTER = "*"
 DEFAULT_SORT = "date"
 DEFAULT_RECURSIVE = False
 
-DOMAIN = "files"
+DOMAIN = "filetrack"
 
 SCAN_INTERVAL = timedelta(minutes=1)
 
@@ -34,63 +36,63 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-def get_files_list(folder_path, filter_term, sort, recursive):
-    """Return the list of files, applying filter."""
-    query = folder_path + filter_term
-    """files_list = glob.glob(query)"""
+def get_files_list(folder_path: str, filter_term: str, sort: str, recursive: bool) -> list[str]:
+    """Return the list of files in folder_path filtered and sorted according to parameters."""
+    query = os.path.join(folder_path, filter_term)
+    files = glob.glob(query, recursive=recursive)
     if sort == "name":
-        files_list = sorted(glob.glob(query, recursive=recursive))
+        return sorted(files)
     elif sort == "size":
-        files_list = sorted(glob.glob(query, recursive=recursive), key=os.path.getsize)
-    else:
-        files_list = sorted(glob.glob(query, recursive=recursive), key=os.path.getmtime)
-    return files_list
+        return sorted(files, key=os.path.getsize)
+    else:  # default to 'date'
+        return sorted(files, key=os.path.getmtime)
 
 
-def get_size(files_list):
-    """Return the sum of the size in bytes of files in the list."""
-    size_list = [os.stat(f).st_size for f in files_list if os.path.isfile(f)]
-    return sum(size_list)
+def get_size(files_list: list[str]) -> int:
+    """Return the total size in bytes of files in the list."""
+    return sum(os.stat(f).st_size for f in files_list if os.path.isfile(f))
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the folder sensor."""
+    """Set up the FileTrack sensor."""
     path = config.get(CONF_FOLDER_PATHS)
     name = config.get(CONF_NAME)
 
     if not hass.config.is_allowed_path(path):
-        _LOGGER.error("folder %s is not valid or allowed", path)
+        _LOGGER.error(
+            "Folder %s is not valid or allowed. Ensure it is under /config/www/", path
+        )
     else:
-        folder = FilesSensor(
+        sensor = FileTrackSensor(
             path,
             name,
             config.get(CONF_FILTER),
             config.get(CONF_SORT),
             config.get(CONF_RECURSIVE),
         )
-        add_entities([folder], True)
+        add_entities([sensor], True)
 
 
-class FilesSensor(Entity):
-    """Representation of a folder."""
+class FileTrackSensor(Entity):
+    """Representation of a folder as a Home Assistant sensor."""
 
     ICON = "mdi:folder"
 
-    def __init__(self, folder_path, name, filter_term, sort, recursive):
-        """Initialize the data object."""
-        folder_path = os.path.join(folder_path, "")  # If no trailing / add it
-        self._folder_path = folder_path  # Need to check its a valid path
+    def __init__(self, folder_path: str, name: str, filter_term: str, sort: str, recursive: bool):
+        """Initialize the FileTrack sensor."""
+        folder_path = os.path.join(folder_path, "")  # Ensure trailing slash
+        self._folder_path = folder_path
         self._filter_term = filter_term
-        self._number_of_files = None
-        self._size = None
-        # self._name = os.path.split(os.path.split(folder_path)[0])[1]
+        self._number_of_files = 0
+        self._size = 0
         self._name = name
         self._unit_of_measurement = "MB"
         self._sort = sort
         self._recursive = recursive
+        self.fileList = []
 
     def update(self):
-        """Update the sensor."""
+        """Update the sensor state."""
         files_list = get_files_list(
             self._folder_path, self._filter_term, self._sort, self._recursive
         )
@@ -99,26 +101,24 @@ class FilesSensor(Entity):
         self._size = get_size(files_list)
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the sensor."""
         return self._name
 
     @property
-    def state(self):
-        """Return the state of the sensor."""
-        decimals = 2
-        size_mb = round(self._size / 1e6, decimals)
-        return size_mb
+    def state(self) -> float:
+        """Return the state of the sensor in MB."""
+        return round(self._size / 1e6, 2)
 
     @property
-    def icon(self):
-        """Icon to use in the frontend, if any."""
+    def icon(self) -> str:
+        """Return the icon to use in the frontend."""
         return self.ICON
 
     @property
-    def extra_state_attributes(self):
-        """Return other details about the sensor state."""
-        attr = {
+    def extra_state_attributes(self) -> dict:
+        """Return additional state attributes."""
+        return {
             "path": self._folder_path,
             "filter": self._filter_term,
             "number_of_files": self._number_of_files,
@@ -127,9 +127,8 @@ class FilesSensor(Entity):
             "sort": self._sort,
             "recursive": self._recursive,
         }
-        return attr
 
     @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
+    def unit_of_measurement(self) -> str:
+        """Return the unit of measurement."""
         return self._unit_of_measurement
